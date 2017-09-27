78bceb63fa398f4b2bde187e6b39dfe4a42980a3
rodzhers69@gmail.com <rodzhers69@gmail.com>
2017-09-27 14:08:32 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#3. piemeers
+NAME="Vards Uzvards"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemeers
+#NAME="Vards Uzvards"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvards Vards"
+#echo $NAME
+
+#1. piemeers
+#NAME="Vards Uzvards"
+#echo $NAME
+
+#0. piemeers
+#history > history_20170927.txt
+
