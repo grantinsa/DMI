5a6ec463d68e4e4737e602886e79aac1204aceae
rodzhers69@gmail.com <rodzhers69@gmail.com>
2017-09-27 16:09:03 +0300

    20170927_16_10

--- shell_examples.sh
+++ shell_examples.sh
@@ -1,10 +1,51 @@
 #!/bin/bash
 
+#7.piemeers
+#skaitliskaa_vertiba='expr 2 + 2'
+#echo "Summas vertiba: "$skaitliskaa_vertiba
+#echo "Summas vertiba: $skaitliskaa_vertiba"
+skaitliskaa_vertiba=`expr 2 + 2`
+echo "Summas vertiba: $skaitliskaa_vertiba"
+echo "Summas vertiba: "$skaitliskaa_vertiba
+#skaitliskaa_vertiba=exrp 2 + 2
+#echo "Summas vertiba: $skaitliskaa_vertiba
+#echo "Summas vertiba: $skaitliskaa_vertiba"
+
+
+#6.piemeers
+#echo $*
+#echo "-------------"
+#kartas_numurs=1
+#for arguments in $*
+#do
+#    echo $kartas_numurs". arguments - "$arguments
+#    kartas_numurs=$kartas_numurs+1
+#done
+
+#5.piemeers
+#echo "Skriptam nodoto argumentu skaits: " $#
+#echo "Argumentu saraksts (attelosanas veids 1): "$*
+#echo "Argumentu saraksts (attelosanas veids 2): "$@
+#echo "Pirma argumenta vertiba: "$1
+#echo "Otra argumenta vertiba: "$2
+#echo $1$2
+
+#4.piemeers
+#echo "Izpildama skripta faila nosaukums" $0
+##echo $n
+#echo "Skriptam nodoto argumentu skaits: " $#
+#echo "Argumentu saraksts (attelosanas veids 1): "$*
+#echo "Argumentu saraksts (attelosanas veids 2): "$@
+#ech "Ieprieksejas komandas izpildes rezultats: "$?
+#echo "Ieprieksejas komandas izpildes rezultats: "$?
+#echo "Skripta izpildei pieskirtais procesa numurs: "$$
+##echo ""$!
+
 #3. piemeers
-NAME="Vards Uzvards"
-echo $NAME
-unset NAME
-echo $NAME
+#NAME="Vards Uzvards"
+#echo $NAME
+#unset NAME
+#echo $NAME
 
 #2. piemeers
 #NAME="Vards Uzvards"
